2b2cf236b1a69e4a73014715731a05a542b4e697
TottyAmadeus <pedrosousa084@hotmail.com>
2022-04-23 12:28:21 +0100

    made it functional, missing color changing and interation counter

--- sorter.py
+++ sorter.py
@@ -1,3 +1,4 @@
+from hashlib import algorithms_guaranteed
 import pygame
 import random
 import math
@@ -42,14 +43,13 @@ class DrawInformation:
         self.start_x = 10
 
 
-
 def map_range(val, in_min, in_max, out_min, out_max):
     return (val - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
-def draw(drawInfo, asc):
+def draw(drawInfo, sort, algo, asc):
     drawInfo.window.fill(drawInfo.BG_COLOR)
-    text = drawInfo.FONT.render(f"Sorting Algorithm: INSERIR AQUI | Order: {'Ascending' if asc else 'Descending'}", 1, drawInfo.WHITE)
+    text = drawInfo.FONT.render(f"Sorting Algorithm: {algo.__name__} | Order: {'Ascending' if asc else 'Descending'} | {'Sorting...'if sort else 'In Standby'}", 1, drawInfo.WHITE)
     drawInfo.window.blit(text, (6, 5))
     draw_list(drawInfo)
     pygame.display.update()
@@ -68,6 +68,7 @@ def draw_list(drawInfo):
         pygame.draw.rect(drawInfo.window, color,
                          (x, y, drawInfo.bar_width, drawInfo.height))
 
+
 def gen_list(n, min_val, max_val):
     lst = []
 
@@ -76,6 +77,25 @@ def gen_list(n, min_val, max_val):
         lst.append(val)
 
     return lst
+  
+
+def bubble_sort(drawInfo, asc = True):
+    lst = drawInfo.lst
+    for i in range(len(lst) -1):
+        for j in range(0, len(lst) - 1 - i):
+            if (lst[j] > lst[j+1] and asc) or (lst[j] < lst[j+1] and not asc):
+                 lst[j] , lst[j+1] = lst[j+1] , lst[j]
+                 yield True
+
+def selection_sort(drawInfo, asc = True):
+    lst = drawInfo.lst
+    for i in range(len(lst)):
+        index = i
+        for j in range(i+1, len(lst)):
+            if (lst[index] > lst[j] and asc) or (lst[index] < lst[j] and not asc):
+                index = j
+        lst[i] , lst[index] = lst[index] , lst[i]
+        yield True
 
 
 def main():
@@ -85,6 +105,11 @@ def main():
     max_val = 100
     sorting = False
     ascending = True
+
+    algorithm_list = [bubble_sort, selection_sort]
+    algorithm_num = 0
+    algorithm = bubble_sort
+    sorting_generator = None
     lst = gen_list(n, min_val, max_val)
     drawInfo = DrawInformation(800, 600, lst)
     clock = pygame.time.Clock()
@@ -92,7 +117,14 @@ def main():
     while running:
         clock.tick(60)
 
-        draw(drawInfo, ascending)
+        if sorting:
+            try:
+                next(sorting_generator)
+            except StopIteration:
+                sorting = False
+        
+        draw(drawInfo, sorting, algorithm, ascending)
+
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -109,13 +141,25 @@ def main():
                     lst = gen_list(n, min_val, max_val)
                     drawInfo.set_list(lst)
                     sorting = False
+
                 if key[pygame.K_RETURN] and not sorting:
                     sorting = True
+                    sorting_generator = algorithm(drawInfo, ascending)
+
                 if key[pygame.K_UP] and not sorting:
                     ascending = True
                 if key[pygame.K_DOWN] and not sorting:
                     ascending = False
-                    
+                if key[pygame.K_LEFT] and not sorting:
+                    algorithm_num -= 1
+                    if algorithm_num < 0:
+                        algorithm_num = 0
+                    algorithm = algorithm_list[algorithm_num]
+                if key[pygame.K_RIGHT] and not sorting:
+                    algorithm_num += 1
+                    if algorithm_num > 1:
+                        algorithm_num = 1
+                    algorithm = algorithm_list[algorithm_num]
 
     pygame.quit()
 
